37a112989334e6879f0d0c0bb4e9eed0bb7c5ad1
ariesman <1609289611@qq.com>
2016-10-31 14:29:52 +0800

    '123'

--- js/companyprofile.js
+++ js/companyprofile.js
@@ -20,7 +20,47 @@ $(document).ready(function(){
      	$(".listone li").css("background","#fff")
      	$(this).css("background","#f0c310")
      })	
-	
+//parttop减开始
+    $(".pageselect span").eq(0).click(function(){
+    	var now=parseInt($(".pageselect span").eq(2).text())
+    	if (now-1<=0) {
+    		now=1
+    	}
+    	$(".pageselect span").eq(2).text("0"+(now-1))
+ 	
+    })
+//减结束   	
+//加开始
+    $(".pageselect span").eq(1).click(function(){
+    	var now=parseInt($(".pageselect span").eq(2).text())
+    	if (now+1>=3) {
+    		now=2
+    	}
+    	$(".pageselect span").eq(2).text("0"+(now+1))
+ 	
+    })
+//加结束     
+
+//partbottom减开始
+    $(".pageselect span").eq(4).click(function(){
+    	var now=parseInt($(".pageselect span").eq(6).text())
+    	if (now-1<=0) {
+    		now=1
+    	}
+    	$(".pageselect span").eq(6).text("0"+(now-1))
+ 	
+    })
+//减结束   	
+//加开始
+    $(".pageselect span").eq(5).click(function(){
+    	var now=parseInt($(".pageselect span").eq(6).text())
+    	if (now+1>=3) {
+    		now=2
+    	}
+    	$(".pageselect span").eq(6).text("0"+(now+1))
+ 	
+    })
+//加结束  
 })
 
 
